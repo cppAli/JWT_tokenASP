72555ff376201ff8096b454bc66bfdb23a9442fe
Alice <romankoalice@gmail.com>
2024-05-17 22:36:16 +0300

    add function controllers (get, post) for jwt

--- JWT_tokenASP/Controller/EmployeeController.cs
+++ JWT_tokenASP/Controller/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JWT_tokenASP.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JWT_tokenASP.Controller
@@ -7,5 +9,25 @@ namespace JWT_tokenASP.Controller
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        [Authorize]
+        [HttpGet]
+        [Route("GetData")]
+        public string GetData()
+        {
+            return "Authenticated with JWT";
+        }
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticated with JWT";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string AddUser(Users user)
+        {
+            return "User added with username " + user.Username;
+        }
     }
 }
